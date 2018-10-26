31ccaf7008a57c380dec382af0a3ab97735d83a5
Kadir Yıldız <47683450074@gumruk.local>
2018-10-26 14:30:56 +0300

    variable definations is done
    All variables define and publish github develop_issue_2

--- JavaApplication86/src/javaapplication86/JavaApplication86.java
+++ JavaApplication86/src/javaapplication86/JavaApplication86.java
@@ -15,7 +15,7 @@ public class JavaApplication86 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a=5;
     }
     
 }
